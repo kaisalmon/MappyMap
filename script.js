67c889c8265c3bec31f825af7d7c5e5269ceb2ac
Your Name <you@example.com>
2017-04-27 18:02:43 +0200

    cooL

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var rnd = new RNG(100);
 var sea_level = 0.3;
+var mount_level = 0.04;
 var w = 30, h = 30;
 var compactness = 0.3;
-var seed = Math.random();
+var seed = 19;
 
-function create_map(){
+var TERRAIN = {
+    'sea':{'opacity':0.1, 'background':'blue'},
+    'grassland':{'opacity':1, 'background':'#26A65B', 'has_region':true},
+    'mountain':{'opacity':1, 'background':'brown', 'has_region':true},
+}
+
+function create_map(blank){
+    if(blank)$('.main').remove();
+
     rnd = new RNG(seed);
     var layers = [
         [ 30,  30, 0.1],
@@ -20,6 +29,8 @@ function create_map(){
         n = add(n, scale(new_n, w, h));
     }
 
+    var m = dynamic_threshold(n, mount_level);
+    
     var o = radial(w, h);
     var os = compactness
     n = add(
@@ -27,35 +38,40 @@ function create_map(){
         mult(o, os)
     );
     n = dynamic_threshold(n, sea_level);
+    var terrain = create_terrain(n, m);
 
-    /*
-    cont = shrink(n);
+    var cont = subtract(n, m); 
+    display(cont);
+    cont = shrink(cont);
+    cont = grow(cont);
     cont = create_regions(cont);
-    n = add(n, cont);
-    n = expand_regions(n);
-    */
-    display(n);
+
+    terrain = add(terrain, cont);
+    display(terrain);
+    terrain = expand_regions(terrain);
+    display(terrain);
+
 }
 $(document).ready(function(){
     create_map();
-    var settings = $('<div/>').appendTo('body');
+    var settings = $('<div/>').prependTo('body');
 
     $('<button/>').text("Reseed").appendTo(settings).click(function(){
         seed = Math.random();
-        create_map();
+        create_map(true);
     });
     settings.append('<br>');
     $('<label/>').text("Sea Level:").appendTo(settings);
     $('<input type="range" min="0" max="1" step="0.05">').val(sea_level).appendTo(settings).bind('change', function(){
         sea_level = $(this).val();
-        create_map();
+        create_map(true);
     });
     settings.append('<br>');
 
     $('<label/>').text("Compactness:").appendTo(settings);
     $('<input type="range" min="0" max="1" step="0.05">').val(compactness).appendTo(settings).bind('change', function(){
         compactness = $(this).val();
-        create_map();
+        create_map(true);
     });
     settings.append('<br>');
 });
@@ -127,29 +143,77 @@ function scale(s, w, h){
 function display(n){
     var w = n.length
     var h = n[0].length
-     $('.main').empty()
+    var $main = $('<div class="main"/>').appendTo('body');
     for(y=0; y < h; y++){
         for(x=0; x < w; x++){
+            if(n[x][y] === undefined){
+                throw("Undefined tile");
+            }
+
             if($.isNumeric(n[x][y])){
                 var c = parseFloat(n[x][y]);
-                $('<div/>').appendTo($('.main')).css("opacity",c);
-            }else{
-                var t = $('<div/>').appendTo($('.main')).css("opacity","1");
-                t.css('background',"#444");
+                $('<div/>').appendTo($main).css("opacity",c);
+            }else if(n[x][y].r ||n[x][y].b || n[x][y].g){
                 var c = 'rgb('+n[x][y]['r']+', '+n[x][y]['g']+', '+n[x][y]['b']+')';
+                var t = $('<div/>').appendTo($main).css("opacity","1").css('background', c);
 
-                if(!n[x][y-1] || !n[x][y-1].r || n[x][y-1] != n[x][y])
-                    t.css('border-top',"2px solid "+c);
-                if(!n[x][y+1] || !n[x][y+1].r || n[x][y+1] != n[x][y])
-                    t.css('border-bottom',"2px solid "+c);
-                if(!n[x+1] || !n[x+1][y].r || n[x+1][y] != n[x][y])
-                    t.css('border-right',"2px solid "+c);
-                if(!n[x-1] || !n[x-1][y].r || n[x-1][y] != n[x][y])
-                    t.css('border-left',"2px solid "+c);
+            }else if(n[x][y].terrain){
+                var t = $('<div/>').appendTo($main).css("opacity","1");
+                
+                if(TERRAIN[n[x][y].terrain]){
+                    t.css('background',TERRAIN[n[x][y].terrain].background);;
+                    t.css('opacity',TERRAIN[n[x][y].terrain].opacity);;
+                }
+                
+                if(n[x][y].region){
+                    var reg = n[x][y].region
+                    var c = 'rgb('+reg.r+', '+reg.g+', '+reg.b+')';
+
+                    if(!n[x][y-1]|| n[x][y-1].region != n[x][y].region)
+                        t.css('border-top',"2px solid "+c);
+                    if(!n[x][y+1] || n[x][y+1].region != n[x][y].region)
+                        t.css('border-bottom',"2px solid "+c);
+                    if(!n[x+1] || n[x+1][y].region != n[x][y].region)
+                        t.css('border-right',"2px solid "+c);
+                    if(!n[x-1] || n[x-1][y].region != n[x][y].region)
+                        t.css('border-left',"2px solid "+c);
+                }
             }
         }
         $('.main').append("</br>")
     }
+}
+
+function create_terrain(land, mount){
+    var w = land.length
+    var h = land[0].length
+    var n = []
+    for(x=0; x < w; x++){
+        n[x] = []
+        for(y=0; y < h; y++){
+            if(land[x][y]){
+                if(mount[x][y])
+                    n[x][y] = {terrain:"mountain"}
+                else
+                    n[x][y] = {terrain:"grassland"}
+            }else
+                n[x][y] = {terrain:"sea"}
+        }
+    }
+    return n;
+}
+
+function subtract(n1, n2){
+    var w = n1.length
+    var h = n1[0].length
+    var n = []
+    for(x=0; x < w; x++){
+        n[x] = []
+        for(y=0; y < h; y++){
+            n[x][y] = Math.max(n1[x][y] - n2[x][y], 0);
+        }
+    }
+    return n;
 }
 
 function add(n1, n2){
@@ -159,8 +223,13 @@ function add(n1, n2){
     for(x=0; x < w; x++){
         n[x] = []
         for(y=0; y < h; y++){
-            if($.isNumeric(n2[x][y])){
+            if(n2[x][y] === 0){
+                n[x][y] = n1[x][y];
+            }else if($.isNumeric(n2[x][y])){
                 n[x][y] = n1[x][y] + n2[x][y];
+            }else if(n2[x][y].r || n2[x][y].g || n2[x][y].b){
+                n[x][y] = n1[x][y];
+                n[x][y].region = n2[x][y];
             }else{
                 n[x][y] = n2[x][y];
             }
@@ -176,7 +245,13 @@ function expand_regions(s){
     for(x=0; x < w; x++){
         n[x] = []
         for(y=0; y < h; y++){
-            n[x][y] = s[x][y] == 1 ? get_closest(s,x,y) : s[x][y];
+            n[x][y] = {};
+            n[x][y].terrain = s[x][y].terrain;
+            if(TERRAIN[n[x][y].terrain].has_region)
+                n[x][y].region = s[x][y].region;
+            if(TERRAIN[n[x][y].terrain].has_region && !n[x][y].region)
+                n[x][y].region = get_closest(s, x, y);
+
         }
     }
     console.log(n);
@@ -185,8 +260,8 @@ function expand_regions(s){
 
 function get_closest(n, x, y){
     return spiral(n, x,y,function(n,x,y){
-        if(n[x] && n[x][y] && n[x][y].r){
-            return n[x][y];
+        if(n[x] && n[x][y] && n[x][y].region){
+            return n[x][y].region;
         }else{
             return false;
         }
@@ -205,6 +280,38 @@ function mult(s, factor){
     }
     return n;
 } 
+
+function grow(s){
+    var w = s.length
+    var h = s[0].length
+    var n = []
+    for(x=0; x < w; x++){
+        n[x] = []
+        for(y=0; y < h; y++){
+            n[x][y] = s[x][y];
+            if(n[x][y]==0){
+                if(x!=0 && s[x-1][y])
+                    n[x][y] = 1;
+                if(x!=w-1 && s[x+1][y])
+                    n[x][y] = 1;
+                if(y!=h-1 && s[x][y+1])
+                    n[x][y] = 1;
+                if(y!=0 && s[x][y-1])
+                    n[x][y] = 1;
+
+                if(y!=0 && x!=0 && s[x-1][y-1])
+                    n[x][y] = 1;
+                if(y!=h-1 && x!=0 && s[x-1][y+1])
+                    n[x][y] = 1;
+                if(y!=0 && x!=w-1 && s[x+1][y-1])
+                    n[x][y] = 1;
+                if(y!=h-1 && x!=w-1 && s[x+1][y+1])
+                    n[x][y] = 1;
+            }
+        }
+    }
+    return n;
+}
 
 function shrink(s){
     var w = s.length
@@ -259,7 +366,7 @@ function create_regions(s){
     for(x=0; x < w; x++){
         n[x] = []
         for(y=0; y < h; y++){
-            n[x][y] = s[x][y];
+            n[x][y] = JSON.parse(JSON.stringify(s[x][y]));
         }
     }
     for(x=0; x < w; x++){
@@ -267,7 +374,7 @@ function create_regions(s){
             if(colors.length == 0){
                 colors = JSON.parse(JSON.stringify(cbackup));
             }
-            if(n[x][y] == 1){
+            if(n[x][y]){
                 floodfill(n, x, y, n[x][y], colors.shift())
             }
         }
@@ -276,8 +383,8 @@ function create_regions(s){
 }
 
 function floodfill(n, x, y, from, to){
-    if(!n[x] || !n[x][y] || n[x][y] !== from) return;
-    n[x][y] = to; 
+    if(!n[x] || !n[x][y] || n[x][y]!== from) return;
+    n[x][y]= to; 
     floodfill(n, x+1, y, from, to);
     floodfill(n, x, y+1, from, to);
     floodfill(n, x-1, y, from, to);
